cef34fc35de21fc7131026aad2b8d1de0e1f558d
Evan Porter <evan.porter@dotman.ca>
2023-01-03 22:24:43 -0400

    Update header styles a little

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,16 +3,16 @@
 </script>
 
 <header class="bg-blue-500 text-white font-extrabold uppercase relative flex flex-wrap items-center justify-between">
-	<div class="text-xl">
+	<div class="px-3 py-2 text-xl w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
 		evanepio.ca
 	</div>
-	<nav>
-		<ul>
+	<nav class="lg:flex flex-grow items-center">
+		<ul class="flex flex-col lg:flex-row list-none ml-auto">
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/" class="hover:opacity-75">Home</a>
+				<a href="/" class="px-3 py-2 hover:opacity-75 flex items-center">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about" class="hover:opacity-75">About</a>
+				<a href="/about" class="px-3 py-2 hover:opacity-75 flex items-center">About</a>
 			</li>
 		</ul>
 	</nav>
