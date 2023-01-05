c873e34fc68626dd26a38b2228e055854eef6aa5
Evan Porter <evan.porter@dotman.ca>
2023-01-04 22:20:03 -0400

    Add lorem ipsum

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 	</h1>
 
 	<p>
-		Stuff
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem provident sunt rerum nihil amet laborum nesciunt asperiores dolorum voluptatem quisquam consequatur est voluptatum voluptate, ratione incidunt recusandae nulla deleniti quia.
 	</p>
 
 </section>
