136a3d3fce51ed07a14cba925f1fa7d07365db6b
Evan Porter <evan.porter@dotman.ca>
2023-01-07 17:07:03 -0400

    Add third page to make nav bar look better.
    It will be replaced later.

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,6 +3,7 @@
 
 	const links: {name: string, url: string}[] = [
 		{name: "Home", url: "/"},
+		{name: "Stuff", url: "/stuff"},
 		{name: "About", url: "/about"}
 	];
 </script>
---
+++ src/routes/stuff/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Stuff</title>
+	<meta name="description" content="Stuff" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Stuff</h1>
+
+	<p>
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae voluptates alias similique? Nesciunt, nostrum? Mollitia amet perferendis labore architecto eveniet tenetur harum minus dicta debitis, ipsam nam eaque unde repudiandae.
+	</p>
+</div>
