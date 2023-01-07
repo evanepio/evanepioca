17d26cbfa54bdc9fafc604e70e80d2fbce3e4c06
Evan Porter <evan.porter@dotman.ca>
2023-01-07 13:23:11 -0400

    Reduce chance of missing utility classes in items

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+
+	const links: {name: string, url: string}[] = [
+		{name: "Home", url: "/"},
+		{name: "About", url: "/about"}
+	];
 </script>
 
 <header class="bg-blue-500 text-white font-extrabold uppercase relative flex flex-wrap items-center justify-between rounded-lg m-2">
@@ -8,12 +13,11 @@
 	</div>
 	<nav class="lg:flex flex-grow items-center">
 		<ul class="flex flex-col lg:flex-row list-none ml-auto p-1">
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/" class="px-3 py-2 hover:opacity-75 flex items-center border-solid border-white border-2 m-1 rounded-lg lg:border-blue-500 lg:hover:border-white">Home</a>
+			{#each links as link}
+			<li aria-current={$page.url.pathname === link.url ? 'page' : undefined}>
+				<a href="{link.url}" class="px-3 py-2 hover:opacity-75 flex items-center border-solid border-white border-2 m-1 rounded-lg lg:border-blue-500 lg:hover:border-white">{link.name}</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about" class="px-3 py-2 hover:opacity-75 flex items-center border-solid border-white border-2 m-1 rounded-lg lg:border-blue-500 lg:hover:border-white">About</a>
-			</li>
+			{/each}
 		</ul>
 	</nav>
 
