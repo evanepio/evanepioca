d090551ed3a8c6ac865054aa1d934f54a096e4dd
Evan Porter <evan.porter@dotman.ca>
2023-01-08 20:40:08 -0400

    Fix footer size to match header links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 		<slot />
 	</main>
 
-	<footer class="p-1 bg-blue-500 text-white font-bold rounded-lg m-2">
+	<footer class="p-2 bg-blue-500 text-white font-bold rounded-lg m-2">
 		<p class="text-center">&copy; 2023 Evan Porter</p>
 	</footer>
 </div>
