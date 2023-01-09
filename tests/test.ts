e3afac3ad62090e039b4ad74cd7786e32bf580cd
Evan Porter <evan.porter@dotman.ca>
2023-01-08 20:00:01 -0400

    Fix playwright tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,11 @@
 import { expect, test } from '@playwright/test';
 
+test('home page has expected h1', async ({ page }) => {
+	await page.goto('/');
+	expect(await page.textContent('h1')).toBe('Home');
+});
+
 test('about page has expected h1', async ({ page }) => {
 	await page.goto('/about');
-	expect(await page.textContent('h1')).toBe('About this app');
+	expect(await page.textContent('h1')).toBe('About');
 });
