ded8c210a36b862fa54cf45b752c505ac257fde1
Evan Porter <evan.porter@dotman.ca>
2023-01-09 23:10:22 -0400

    Attempt to add firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp, type FirebaseApp } from 'firebase/app';
+import { Firestore, getFirestore } from 'firebase/firestore';
+import { getAuth, type Auth } from 'firebase/auth';
+
+// Initialize Firebase
+const app: FirebaseApp = initializeApp(
+    {
+        apiKey: "AIzaSyCegRf8GZYR9yGo3QWeoRUQrs9B1GvabP4",
+        authDomain: "example-c76ca.firebaseapp.com",
+        projectId: "example-c76ca",
+        storageBucket: "example-c76ca.appspot.com",
+        messagingSenderId: "773371216860",
+        appId: "1:773371216860:web:b0ef8bbd6ce51ffa6b0519",
+        measurementId: "G-ZQ17N5PNFL"
+    }
+);
+
+export const db: Firestore = getFirestore(app);
+export const auth: Auth = getAuth(app);
