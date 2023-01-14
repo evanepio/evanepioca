8da5f25b48f934398b79134b1a28736a12bbc3d1
Evan Porter <evan.porter@dotman.ca>
2023-01-13 22:54:38 -0400

    Retrieve stuff from firebase

--- src/routes/stuff/+page.svelte
+++ src/routes/stuff/+page.svelte
@@ -2,6 +2,7 @@
 	import { auth, db, provider } from '$lib/firebase';
 	import FirebaseApp from 'sveltefire/FirebaseApp.svelte';
 	import User from 'sveltefire/User.svelte';
+	import Collection from 'sveltefire/Collection.svelte'
 	import { signInWithPopup, signOut } from 'firebase/auth';
 </script>
 
@@ -16,8 +17,19 @@
 	<FirebaseApp {auth} firestore={db}>
 		<User let:user>
 			<p>
-				Hello {user.displayName} ({user.uid}).
+				Hello, {user.displayName}!
 			</p>
+
+			<Collection ref="stuff" let:data let:count>
+				<p class="italic m-2">Fetched {count} documents</p>
+				
+				<dl class="border-solid border-yellow-500 bg-yellow-500 border-4 rounded m-4 p-5">
+				{#each data as post}
+				  <dt class="font-bold">{post.suggested_date.toDate().toISOString()} - {post.title}</dt>
+				  <dd class="ml-4">{post.summary}</dd>
+				{/each}
+			    </dl>
+			</Collection>
 
 			<button
 				class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
